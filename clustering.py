1e3a7d2c8db13c3c843d3f0aeb50370170d7dd3a
sudarshanvankudre <joecurious7@gmail.com>
2021-04-07 01:26:48 -0700

    Removed comments

--- clustering.py
+++ clustering.py
@@ -30,33 +30,12 @@ def preclustering(X):
 
 def labels_array(X):
     """Returns an array of indices indicating which cluster the corresponding sample of X belongs to"""
-    # best_silhouette_score = -1
-    # best_cluster_labels = None
     num_clusters = 1000
     clusterer = MiniBatchKMeans(n_clusters=num_clusters, random_state=10, compute_labels=True)
     clusterer.fit(X)
     cluster_labels = clusterer.labels_
     silhouette_avg = silhouette_score(X, cluster_labels)
     print("Silhouette score:", silhouette_avg)
-    # max_n_clusters = 500
-    # min_n_clusters = 100
-    # cluster_size_increment = 50
-    # silhouette_scores = []
-    # for n_clusters in range(min_n_clusters, max_n_clusters + 1, cluster_size_increment):
-    #     # print("Num of clusters: {}".format(n_clusters))
-    #     clusterer = MiniBatchKMeans(n_clusters=n_clusters, random_state=10, compute_labels=True)
-    #     clusterer.fit(X)
-    #     cluster_labels = clusterer.labels_
-    #     silhouette_avg = silhouette_score(X, cluster_labels)
-    #     silhouette_scores.append(silhouette_avg)
-    #     if silhouette_avg > best_silhouette_score:
-    #         best_silhouette_score = silhouette_avg
-    #         best_cluster_labels = cluster_labels
-    # plt.title("Num Dimensions: {}".format(X.shape[1]))
-    # plt.xlabel("Num Clusters")
-    # plt.ylabel("Silhouette Score")
-    # plt.plot(np.arange(min_n_clusters, max_n_clusters + 1, cluster_size_increment), silhouette_scores)
-    # plt.savefig("clustering_evals/clustering_eval.png")
     return cluster_labels
 
 
