5d48c4b9d7f7c3e62fa9150cb627dbf6e9d4fc2b
sudarshanvankudre <joecurious7@gmail.com>
2021-04-07 23:28:26 -0700

    removed device configuration

--- model.py
+++ model.py
@@ -2,8 +2,6 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 
 class FCPokeNet(nn.Module):
     def __init__(self):
